417a560373efca64e9c78994668e797940f67cf9
Eirik B <87980291+Eiriksb@users.noreply.github.com>
2022-09-01 16:41:23 +0200

    Database connected

---
+++ BlazorTipz/Data/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorTipz.Data
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string f_name { get; set; }
+        public string l_name { get; set; }
+
+    }
+}
---
+++ BlazorTipz/Startup.cs
@@ -0,0 +1,31 @@
+﻿using DataLibrary;
+using Microsoft.AspNetCore.Builder;
+
+namespace BlazorTipz
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration condiguration)
+        {
+            Configuration = condiguration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<Data.WeatherForecastService>();
+            services.AddSingleton<IDataAccess, DataAccess>();
+        }
+
+        
+    }
+}
