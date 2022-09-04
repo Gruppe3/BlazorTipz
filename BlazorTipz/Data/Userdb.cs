d02f680bc32f49523c6f7e1b40f5fe4055be8e1d
Trivinyx <tamyre@uia.no>
2022-09-05 00:25:25 +0200

    needs som fixes from after sql query on login

---
+++ BlazorTipz/Data/Userdb.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTipz.Data
+{
+    public class Userdb
+    {
+        public int? employmentId { get; set; }
+        public byte[] passwordHash { get; set; }
+        public byte[] passwordSalt { get; set; }
+        public string fName { get; set; } = string.Empty;
+        public string lName { get; set; } = string.Empty;
+        public string passHash { get; set; }
+        public string passSalt { get; set; }
+    }
+}
