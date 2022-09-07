cf9f593a7ea766fcadf48d55a956642b99548e5c
Eirik B <87980291+Eiriksb@users.noreply.github.com>
2022-09-08 01:01:56 +0200

    Settings added
    Can change firstname and lastname on logged in user

--- BlazorTipz/Data/UserDto.cs
+++ BlazorTipz/Data/UserDto.cs
@@ -3,6 +3,9 @@
     public class UserDto
     {
         public int? employmentId { get; set; }
+        public string fName { get; set; } = string.Empty;
+        public string lName { get; set; } = string.Empty;
+        
         public string password { get; set; } = string.Empty;
         public string role { get; set; } = "User";
     }
