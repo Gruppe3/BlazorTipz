51a8545907922bb0fd6cb74df8da13c4a01d5bff
Eirik B <87980291+Eiriksb@users.noreply.github.com>
2022-09-29 17:01:35 +0200

    Added Interface USerManager and UserManager

---
+++ BlazorTipz/ViewModels/UserManager.cs
@@ -0,0 +1,47 @@
+﻿using BlazorTipz.Models;
+using BlazorTipz.ViewModels.User;
+using DataLibrary;
+
+namespace BlazorTipz.ViewModels
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IDataAccess _data;
+        private readonly IConfiguration _config;
+
+        public UserManager(IDataAccess data, IConfiguration connectionString)
+        {
+            _data = data;
+            _config = connectionString;
+        }
+        public async Task Login(UserViewmodel user, out string token, out string err)
+        {
+            token = "";
+            err = "";
+            try
+            {
+                var sql = "SELECT * FROM Users WHERE employmentId = @empid;";
+
+                UserDb dbinfo = await _data.LoadData<UserDb, dynamic>(sql, new { empid = user.employmentId }, _config.GetConnectionString("default"), true);
+                if (dbinfo == null)
+                {
+                    err = "User not found";
+                    return;
+                }
+                if (!VerifyPasswordHash(user.password, dbinfo.passwordHash, dbinfo.passwordSalt))
+                {
+                    err = "Wrong password";
+                    return;
+                }
+                token = GenerateToken(dbinfo);
+            }
+            catch (Exception ex)
+            {
+                err = ex.Message;
+            }
+        }
+        {
+
+        }
+    }
+}
