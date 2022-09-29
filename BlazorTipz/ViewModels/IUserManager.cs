0da33e4eff396be52e8fb95d24ab92ff0124e56a
Trivinyx <tom.a.s.myre@gmail.com>
2022-09-29 16:51:05 +0200

    Created IUserManager
    Interface

---
+++ BlazorTipz/ViewModels/IUserManager.cs
@@ -0,0 +1,11 @@
+﻿using BlazorTipz.Data;
+using BlazorTipz.ViewModels.User;
+
+namespace BlazorTipz.ViewModels
+{
+    public interface IUserManager
+    {
+        Task Login(UserViewmodel user, out string token, out string err);
+        
+    }
+}
