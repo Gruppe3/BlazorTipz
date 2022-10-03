e3a52d2daa399ff4ac34b46cb3c702462b2372a0
Trivinyx <tom.a.s.myre@gmail.com>
2022-10-03 16:49:30 +0200

    Cleanup

--- BlazorTipz/Data/Role.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorTipz.Data
-{
-    public class Role
-    {
-        public int? roleId { get; set; }
-        public string role { get; set; }
-    }
-}
--- BlazorTipz/Data/TeamDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorTipz.Data
-{
-    public class TeamDto
-    {
-        public string teamName { get; set; }
-        public int teamLeader { get; set; }
-        public int? teamid { get; set; }
-    }
-}
