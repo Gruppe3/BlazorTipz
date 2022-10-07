c0e79ec5ffbe1f9445165328e4826e20bb7fa142
Trivinyx <tom.a.s.myre@gmail.com>
2022-10-07 15:32:43 +0200

    In progress, need more troubleshouting on the page.

--- BlazorTipz/ViewModels/User/IUserManager.cs
+++ BlazorTipz/ViewModels/User/IUserManager.cs
@@ -4,10 +4,26 @@ namespace BlazorTipz.ViewModels.User
 {
     public interface IUserManager
     {
+        UserViewmodel? CurrentUser { get; set; }
+
+        List<UserViewmodel>? usersToRegister { get; }
+
         //These methods are available for anyone implementing this interface
         UserViewmodel getCurrentUser();
+
+        //first return "string?" = "token", second return "string?" = errorMsg
         Task<(string, string)> Login(UserViewmodel user);
-        Task<string> registerUserSingel(UserViewmodel user);
+        
+        //first return "string?" = errmsg, second return "string?" = sucsessMsg
+        Task<(string?, string?)> registerUserSingel(UserViewmodel user);
+        Task<(string?, string?)> registerMultiple(List<UserViewmodel>? usersToReg);
+
+        //add to list usersToregister, returns either a error or succsess msg
+        string stageToRegisterList(UserViewmodel user);
+
+        //deletes a specified element form usersToRegister list.
+        void deleteFromRegisterList(string emipd);
+
         Task<(UserViewmodel, string)> getCurrentUser(string token);
         void logout();
         Task<string> updateCurrentUser(UserViewmodel user);
