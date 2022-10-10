51049d73e8b6c81ac9a2a349d95bd3d85dbc01a9
Henrik <89202778+HenrikRaakil@users.noreply.github.com>
2022-10-10 14:10:12 +0200

    Created firstTimeLogin check
    - added firstTimeLogin to UserViewModel
    - added firstTimeLogin to UserDb
    - refactored index.razor and added firstTimeLogin check
    - added firstTimeLogin check to UserSettings
    
    Co-authored-by: Glenn 89195051+GlennJoakimB@users.noreply.github.com

--- BlazorTipz/ViewModels/User/UserViewmodel.cs
+++ BlazorTipz/ViewModels/User/UserViewmodel.cs
@@ -12,12 +12,14 @@ namespace BlazorTipz.ViewModels.User
         public string teamId { get; set; }
         public string password { get; set; }
         public string RepeatPassword { get; set; } = string.Empty;
-        public int listnum { get; set; } = 0;   
-        
+        public int listnum { get; set; } = 0;
+        public bool firstTimeLogin{ get; set; }
         public UserViewmodel()
         {
 
         }
+        
+        // passerer inn data fra UserDb og setter lokale verdier 
         public UserViewmodel(UserDb user)
         {
             this.employmentId = user.employmentId;
@@ -25,6 +27,7 @@ namespace BlazorTipz.ViewModels.User
             this.password = user.password;
             this.role = user.role;
             this.teamId = user.teamId;
+            this.firstTimeLogin = user.firstTimeLogin;
         }
     }
 
