d7695394990043f3561d185144dd6ba961e50c35
GlennJoakimB <89195051+GlennJoakimB@users.noreply.github.com>
2022-10-11 13:28:27 +0200

    Early testing
    - Beginning of DummyDBR
    - Beginning of TeamManagerTest

---
+++ BlazorTipzTests/ViewModels/DummyClass/DummyDBR.cs
@@ -0,0 +1,98 @@
+﻿using BlazorTipz.Models;
+using BlazorTipz.Models.DbRelay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTipzTests.ViewModels.DummyClass
+{
+    public class DummyDBR : IDbRelay
+    {
+        public Task addTeamEntry(TeamDb team)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task addUserEntries(List<UserDb> toSaveUsers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task changeTeamsStateTo(List<TeamDb> users, bool state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task changeTeamStateTo(string teamid, bool state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task changeUsersStateTo(List<UserDb> users, bool state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task changeUserStateTo(string empid, bool state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TeamDb>> getActiveTeams()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserDb>> getActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TeamDb>> getInactiveTeams()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserDb>> getInactiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDb> getLoginUser(string empId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TeamDb> getSingleTeamDbFromDb(string teamId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDb> lookUpUser(string empId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task saveSuggestion(SuggestionEntity suggestion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task saveSuggestionList(List<SuggestionEntity> suggestions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task updateTeamEntry(TeamDb team)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task updateUserEntry(UserDb toSaveUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlazorTipzTests/ViewModels/Team/TeamManagerTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlazorTipz.ViewModels.Team;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorTipz.Models;
+using BlazorTipz.Models.DbRelay;
+using BlazorTipz.ViewModels.User;
+using Microsoft.AspNetCore.Routing;
+using BlazorTipzTests.ViewModels.DummyClass;
+
+namespace BlazorTipz.ViewModels.Team.Tests
+{
+    [TestClass()]
+    public class TeamManagerTests
+    {
+        [TestMethod()]
+        public void TeamManagerTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public async void getTeamsTest()
+        {
+            //arrange
+            TeamManager UnitUnderTest = new TeamManager(new DummyDBR(), new DummyUserManager());
+
+
+            //act
+
+            //assert
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getTeamsWithNoTeamTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void updateTeamsListTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void getTeamTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void getInactiveTeamsTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void updateTeamTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void createTeamTest()
+        {
+            
+            
+        }
+    }
+}
