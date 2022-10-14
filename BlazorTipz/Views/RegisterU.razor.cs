2e7a392d04435faba51a5cf656113c32baf14797
Eirik B <87980291+Eiriksb@users.noreply.github.com>
2022-10-15 01:02:51 +0200

    Better Implementation of export

--- BlazorTipz/Views/RegisterU.razor.cs
+++ BlazorTipz/Views/RegisterU.razor.cs
@@ -1,6 +1,9 @@
 using BlazorTipz.Data;
 using Microsoft.AspNetCore.Mvc;
 using BlazorTipz.ViewModels.User;
+using System.Data;
+using ClosedXML.Excel;
+using Microsoft.JSInterop;
 
 namespace BlazorTipz.Views
 {
@@ -57,6 +60,22 @@ namespace BlazorTipz.Views
             return err;
         }
 
+        //Sender resuest til registerUserSingel og eksporter en excel fil
+        public async Task<ActionResult<string>> RegisterUsersWExcel()
+        {
+            (string err, string suc) = await _userM.registerMultiple(null);
+            Checker = err;
+            UsersList = _userM.getRegisterUserList();
+            if (suc != null)
+            {
+                await DownloadExcelDocument();
+                Checker = suc;
+                DialogService.Close();
+                return suc;
+            }
+            return err;
+        }
+
         //Delete user from list
         public async Task<ActionResult<string>> DeleteUser(UserViewmodel request)
         {
@@ -65,6 +84,48 @@ namespace BlazorTipz.Views
             UsersList = _userM.getRegisterUserList();
             //Break foreach loop
             return "User deleted";
+        }
+
+
+        public async Task DownloadExcelDocument()
+        {
+            //Get current date 24hr format
+            string date = DateTime.Now.ToString("dd-MM-yyyy-HH-mm");
+
+            using (var workbook = new XLWorkbook())
+            {
+                IXLWorksheet worksheet =
+                workbook.Worksheets.Add("UserList");
+                worksheet.Cell("A1").Value = "List number";
+                worksheet.Cell("B1").Value = "Employment ID";
+                worksheet.Cell("C1").Value = "Role";
+                worksheet.Cell("D1").Value = "Password";
+                //Set width of columns
+                worksheet.Column(1).Width = 15;
+                worksheet.Column(2).Width = 15;
+                worksheet.Column(3).Width = 10;
+                worksheet.Column(4).Width = 10;
+
+                int row = 2;
+
+                foreach (var item in UsersList)
+                {
+                    worksheet.Cell("A" + row).Value = item.listnum;
+                    worksheet.Cell("B" + row).Value = item.employmentId;
+                    worksheet.Cell("C" + row).Value = item.role.ToString();
+                    worksheet.Cell("D" + row).Value = item.password;
+                    row++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    await JSRuntime.InvokeVoidAsync("saveAsFile",
+                    $"UserList_{date}.xlsx", Convert.ToBase64String(content));
+                }
+            }
+
         }
     }
 }
