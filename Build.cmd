a6be200dedaa1bd8cf6ea09c4c1b2226d3ce2971
Trivinyx <tom.a.s.myre@gmail.com>
2022-11-02 11:11:21 +0100

    Build.cmd works correctly
    builds and publishes application container
    builds a mariadb database, and crates db from BlazorTipzDDL.sql

--- Build.cmd
+++ Build.cmd
@@ -1,17 +1,19 @@
 @echo off
 
-::Docker killing container
+::Docker killing containers
 docker kill ndtipz
-::Build Image specified in Dockerfile
-docker image build -t ndtipz .
-
-::Run container
-docker container run --rm -it -d --name ndtipz --publish 80:80 ndtipz
-
-::Docker killing container
 docker kill mariadb
 
-::Docker update mariadb
+::application BlazorTipz
+::Build application Image specified in Dockerfile
+docker image build -t ndtipz -f BlazorTipz/Dockerfile .
+
+::Run application container
+docker container run --rm -it -d --name ndtipz --publish 80:80 ndtipz
+
+
+::MariaDB database
+::Docker update mariadb libs
 docker pull mariadb:10.5.11
 
 ::Launch mariadb database in Docker on port 3308:3306/tcp
@@ -19,14 +21,6 @@ docker container run --rm -it -d --name mariadb --publish 3308:3306/tcp -e MYSQL
 
 ::Docker copy BlazorTipzDDL.sql into mariadb container
 docker cp BlazorTipzDDL.sql mariadb:/docker-entrypoint-initdb.d/BlazorTipzDDL.sql
-::fetch and run the sql script to create the database
-docker exec -i -u root mariadb mysql -h 127.0.0.1 -p 3308:3306 -pndtipz -f mariadb < BlazorTipzDDL.sql
-
-
-
-
-
-
 
 echo.
 echo "Link: http://localhost:80/"
